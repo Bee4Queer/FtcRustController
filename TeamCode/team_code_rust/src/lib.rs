3dad1cbd9a0e14b5f64745b2d2aaa66441c48f05
Viv <poach-donated-slip@duck.com>
2026-08-01 11:24:38 -0500

    update ftc-rust and cleanup examples a bit

--- TeamCode/team_code_rust/src/lib.rs
+++ TeamCode/team_code_rust/src/lib.rs
@@ -31,7 +31,10 @@ fn my_linear_op_mode(ctx: &ftc::FtcContext) {
     std::thread::sleep(Duration::from_secs_f32(0.5));
 }
 
-/// State used in the iterative op mode. Essentially equivalent to adding properties to a class in java.
+/// State used in the iterative op mode. Essentially equivalent to adding properties to a class in
+/// java. Has to implement Java and not have any non-static references, as well as being Send + Sync
+/// (Send means safe to move across threads, Sync means safe to move references to the type across
+/// threads).
 #[derive(Default)]
 struct IterativeState {
     /// Devices implement Default by returning a null object of sorts that panics
@@ -47,29 +50,26 @@ struct IterativeState {
     group = "Example"
 )]
 fn my_iterative_op_mode(iterative: &ftc::IterativeContext) {
-    iterative.init(|ctx: &ftc::FtcContext| {
-        ctx.with_state(|state: &mut IterativeState| {
-            // equivalent to hardwareMap.get(DcMotor.class, "motor") in Java:
-            state.motor = ctx.hardware().get::<DcMotor>("motor");
-            state.motor.set_direction(ftc::hardware::Direction::Forward);
+    iterative.init(|ctx: &ftc::FtcContext, state: &mut IterativeState| {
+        // equivalent to hardwareMap.get(DcMotor.class, "motor") in Java:
+        state.motor = ctx.hardware().get::<DcMotor>("motor");
+        state.motor.set_direction(ftc::hardware::Direction::Forward);
 
-            ctx.telemetry().add_data("Status", "Initalized");
-            ctx.telemetry().update();
-        });
+        ctx.telemetry().add_data("Status", "Initalized");
+        ctx.telemetry().update();
     });
 
-    iterative.start(|ctx| {
-        // types can be elided sometimes
-        ctx.with_state(|state: &mut IterativeState| {
-            state.motor.set_power(0.5);
-            std::thread::sleep(Duration::from_secs_f32(2.0));
-            state.motor.set_power(0.0);
-        });
+    iterative.start(|_ctx, state: &mut IterativeState| {
+        state.motor.set_power(0.5);
+        std::thread::sleep(Duration::from_secs_f32(2.0));
+        state.motor.set_power(0.0);
     });
 
-    iterative.stop(|ctx| {
+    iterative.stop(|ctx, _state: &mut ()| {
+        // state has to have a type, so use the unit type and ignore the value
         info!("Ran for {:?}!", ctx.runtime());
     });
 
-    // attempting to call wait_for_start with an interative context will immediately return and print a warning
+    // attempting to call wait_for_start with an interative context will immediately return and
+    // print a warning
 }
